2e350bc0358a4081eb5405254dba7b0d100ffa07
Worawut Khumnoi <123399888+Mesww@users.noreply.github.com>
2024-07-15 13:14:16 +0700

    refine navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,18 +1,23 @@
-import { Link } from 'react-router-dom'; // If using React Router
 import gemlogo from '../../assets/Screenshot_2567-07-10_at_12.04.25-removebg.png';
 import "./style.sass";
+import { useState } from 'react';
+
 const Navbar = () => {
-  return (
-    <nav className=" navbar  fixed bottom-5 left-0 right-0 z-50 rounded-full">
-      <div className=" px-4 flex justify-between items-center h-14 ">
-        {/* <Link className="text-white text-xl font-bold" to="/">Logo</Link> */}
-        <div>1</div>
-        <img src={gemlogo} alt="logo" className='   w-14 ' />
-        <div>1</div>
-        
-      </div>
-    </nav>
-  );
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const handleLogoClick = () => {
+        setIsAnimating(prevState => !prevState);
+    };
+
+    return (
+        <nav className={`navbar fixed bottom-5 left-0 right-0 z-50 rounded-full ${isAnimating ? 'navbar-close' : ''}`}>
+            <div className="px-4 flex items-center h-14 navbar-wrapper">
+                <div className={`content ${isAnimating ? 'center-content' : ''}`}>1</div>
+                <img src={gemlogo} alt="logo" className=' mx-auto logo w-20' onClick={handleLogoClick} />
+                <div className={`content ${isAnimating ? 'center-content' : ''}`}>1</div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
